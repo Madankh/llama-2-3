8ba19fadd2d71a954be0f4ef38125c0973355913
Madankh <khadkasuman0000@gmail.com>
2024-10-20 14:13:10 +0545

    model end

---
+++ inference.py
@@ -0,0 +1,14 @@
+from typing import Optional
+import torch
+import time
+from pathlib import Path
+import json
+from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+from model import ModelArgs, Transformer
+
+class LLaMa:
+    def __init__(self, model:Transformer, tokenizer:SentencePieceProcessor, model_args:ModelArgs):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = model_args
--- model.py
+++ model.py
@@ -161,7 +161,21 @@ class FeedForward(nn.Module):
         hidden_dim = 4 * args.dim 
         hidden_dim = int(2*hidden_dim/3)
 
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffm_dim_multiplier * hidden_dim)
+        # Round the hidden_dim to the nearest multiple of hte multiple_of parameters
+        hidden = args.multiple_of * ((hidden + args.multiple_of - 1)//args.multiple_of)
 
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+        
+    def forward(self, x:torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_v = self.w3(x)
+        x = swish * x_v
+        x = self.w2(x)
+        return x
 
 
 class EncoderBlock(nn.Module):
