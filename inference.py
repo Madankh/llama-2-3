e465e2e42765aec926f754373054b3281045c673
Madankh <khadkasuman0000@gmail.com>
2024-10-21 18:59:48 +0545

    inferance

--- inference.py
+++ inference.py
@@ -12,3 +12,24 @@ class LLaMa:
         self.model = model
         self.tokenizer = tokenizer
         self.args = model_args
+
+    @staticmethod
+    def build(checkpoints_dir:str, tokenizer_path:str, load_model:bool, max_seq_len:int, max_batch_size:int , device:str):
+        prev_time = time.time()
+        if load_model:
+            checkpoints = sorted(Path(checkpoints_dir).glob('*.pth'))
+            assert len(checkpoints) > 0, "No checkpoints files found"
+            chk_path = checkpoints[0]
+            print(f"Loading checkpoint {chk_path}")
+            checkpoint = torch.load(chk_path, map_location="cpu")
+            print(f"Loaded checkpoint in {(time.time() - prev_time):.2f}s")
+            prev_time = time.time()
+
+        with open(Path(checkpoints_dir)/"params.json", "r") as f:
+            params = json.loads(f.read())
+        model_args : ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len,
+            max_batch_size=max_batch_size,
+            device=device,
+            **params
+        )
