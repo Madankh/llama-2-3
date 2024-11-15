d0f5fda11da220803cae46822f61f42c1289906d
Madankh <khadkasuman0000@gmail.com>
2024-11-15 14:07:50 +0545

    let goo

--- model2.py
+++ model2.py
@@ -37,18 +37,29 @@ class RMSNorm(torch.nn.Module):
         return output * self.weight
     
 def precompute_freqs_cis(dim:int, end:int, theta:float = 10000.0):
+
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim//2)].float()/dim))
+
     t = torch.arange(end, device=freqs.device)
+
     freqs=  torch.outer(t,  freqs).float()
+
     freqs_cos = torch.cos(freqs) # real part
+
     freqs_sin = torch.sin(freqs) # imaginary part
+
     return  freqs_cos , freqs_sin
 
 def reshape_for_boardcast(freqs_cis:torch.Tensor, x:torch.Tensor):
+
     ndim = x.ndim
+
     assert 0 <= 1 < ndim
+
     assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+
     shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+
     return freqs_cis.view(shape)
     
 def apply_rotary_emb(
@@ -59,20 +70,26 @@ def apply_rotary_emb(
     ) -> Tuple[torch.Tensor, torch.Tensor]:
     # Reshape xq and sk to meatch the complex representation
     xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+
     xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
 
     # Reshape freqs_cos and freqs_sin for broadcasting
     freqs_cos = reshape_for_boardcast(freqs_cos, xq_r)
+
     freqs_sin = reshape_for_boardcast(freq_sin, xq_r)
 
     # apply rotation using real numbers
     xq_out_r = xq_r * freqs_cos - xq_i * freq_sin
+
     xq_out_i = xq_r * freqs_sin - xq_i * freqs_cos
+
     xk_out_r = xk_r * freqs_cos - xk_i * freq_sin
+
     xk_out_i = xk_r * freqs_sin - xk_i * freqs_cos
 
     # flatten last two dimensions 
     xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1).flatten(3)
+    
     xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1).flatten(3)
 
     return xq_out, xk_out
