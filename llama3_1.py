5c988f3c21cba9bfaa9b0136f3ec94c18a54fcb6
Madan Bahadur khadka <84616682+Madankh@users.noreply.github.com>
2024-12-26 22:11:35 +0545

    llama

--- llama3_1.py
+++ llama3_1.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 from typing import Tuple, List, Optional
+import torch.nn.functional as F
 class ModelArgs:
     block_size : int = 8192
     vocab_size : int = 128256
@@ -112,4 +113,90 @@ class CausalSelfAttention(nn.Module):
             self.cache_v = torch.zeros((config.max_gen_batch_size, config.block_size, self.n_kv_head, self.hd))
         
     def forward(self, x, freqs_cis=None, start_pos=None, mask=None):
-        B, T, C = x.size()
+        B, T, C = x.size() # Batch size, seq, embedding dimensionalaity
+        qkv = self.c_atten(x)
+        q,k,v = qkv.split([self.n_head * self.hd, self.n_kv_head * self.hd, self.n_kv_head * self.hd], dim=-1)
+        q ,k ,v = map(lambda x:x.view(B, T, -1, self.hd),(q,k,v)) # (B, T, NH, HD)
+
+        q,k = apply_rotary_emb(q, k, freqs_cis)
+
+        if self.use_kv and not self.traning and start_pos >= 0:
+            self.cache_k[:B, start_pos:start_pos + T] = k
+            self.cache_v[:B, start_pos:start_pos + T] = v
+            k = self.cache_k[:B, : start_pos + T]
+            v = self.cache_v[:B, : start_pos + T]
+        
+        k = repeat_kv(k, self.n_rep)
+        v = repeat_kv(v, self.n_rep)
+
+        q, k , v = map(lambda x:x.transpose(1,2), (q,k,v)) # (B, NH, T, HD)
+
+        if self.flash:
+            y = F.scale_dot_product_attention(q, k, v, mask == 0 if T>1 else None)
+        else:
+            score = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(self.hd))
+            if mask is not None:
+                score = score.masked_fill(mask ==0, float('-inf'))
+            att = F.softmax(score.float(), dim=-1).type_as(score)
+            y = att @ v
+
+        y = y.transpose(1, 2).contiguous().view(B,T,C)
+        return y
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        hidden_dim = 4 * config.n_embd
+        hidden_dim = int(2 * hidden_dim / 3)
+        # custom dim factor multiplier
+        if config.ffn_dim_multiplier is not None:
+            hidden_dim = int(config.ffn_dim_multiplier * hidden_dim)
+        hidden_dim = config.multiple_of * ((hidden_dim + config.multiple_of - 1) // config.multiple_of)
+        self.c_fc = nn.Linear(config.n_embd, hidden_dim, bias=False)
+        self.c_fc2 = nn.Linear(config.n_embd, hidden_dim, bias=False)
+        self.c_proj = nn.Linear(hidden_dim, config.n_embd, bias=False)
+    def forward(self, x):
+        x1 = self.c_fc(x)
+        x2 = self.c_fc2(x)
+        x2 = F.silu(x2)
+        x = x1 * x2
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln1 = RMSNorm(config.n_embd, config.norm_eps)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = RMSNorm(config.n_enbdm config.norm_eps)
+        self.mlp = MLP(config)
+    def forword(self , x, freqs_cis=None, start_pos=None, mask=None):
+        x = x + self.attn(self.ln1(x), freqs_cis, start_pos, mask)
+        x = x + self.mlp(self.ln_2(x))
+        return x
+    
+@dataclass
+class LlamaConfig:
+    version: str = "3.1"
+    block_size: int = 8192
+    vocab_size: int = 128256
+    n_layer: int = 32
+    n_head: int = 32
+    n_kv_head: int = 8
+    n_embd: int = 4096
+    ffn_dim_multiplier: float = 1.3
+    multiple_of: int = 1024
+    norm_eps: float = 1e-5
+    rope_theta: float = 500000.0
+    use_scaled_rope: bool = True
+    max_gen_batch_size: int = 4
+    use_kv: bool = True
+    flash: bool = False  # use flashattention?
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+        assert self.n_kv_head <= self.n_head
+        assert self.n_head % self.n_kv_head == 0
+        assert self.n_embd % self.n_head == 0
