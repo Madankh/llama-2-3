801dff750164f067c980c3cc7d814951da396ed1
madankh <khadkasuman0000@gmail.com>
2025-04-01 17:32:59 +0545

    medicalllm

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+
+torch_version = torch.__version__.split(".")
+# major_torch, minor_torch = int(torch_version[0]), int(torch_version[1])
+
+# print(f"PyTorch Version: {torch.__version__}")
+# print(f"Major: {major_torch}, Minor: {minor_torch}")
